3173e41741f2298fae50c9571c4259800b2874f4
Adam Brandizzi <adam@brandizzi.com.br>
2020-06-19 20:21:18 -0300

    setup.py adjustments: change name, do not use find_packages anymore (it was not working) and create an entry point.

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 from distutils.core import setup
-from setuptools import find_packages
+
 setup(
     name="input-device-indicator",
     version="0.0.1.dev1",
-    author='Adam Victor Brandizzi',
+    author='Adam Brandizzi',
     author_email='adam@brandizzi.com.br',
     description='input-device-indicator',
     license='LGPLv3',
     url='https://github.com/brandizzi/input-device-indicator',
-
-    packages=find_packages(),
+    packages=['inputdeviceindicator'],
+    entry_points={
+        'gui_scripts': [
+            'input-device-indicator=inputdeviceindicator.main:main'
+        ]
+    },
     test_suite='inputdeviceindicator.tests',
     test_loader='unittest:TestLoader',
 )
