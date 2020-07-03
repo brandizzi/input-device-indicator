5c9081d7c3a68ee493b6b9e122e16b3fbd2c62ea
Adam Brandizzi <adam@brandizzi.com.br>
2020-07-03 10:04:19 -0300

    Make tests runnable now.

---
+++ inputdeviceindicator/tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Copyright © 2020 Adam Brandizzi
+#
+# This file is part of Input Device Indicator.
+#
+# Input Device Indicator is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Input Device Indicator is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Input Device Indicator.  If not, see <https://www.gnu.org/licenses/>
+#
+
+from inelegant.finder import TestFinder
+
+
+load_tests = TestFinder(
+    'inputdeviceindicator.command',
+    'inputdeviceindicator.indicator'
+).load_tests
